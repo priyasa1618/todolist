bd1961ec69ad5ff66e981ce1f4eb2abe19a76856
Decoder-X <asakil851@gmail.com>
2026-03-08 16:37:01 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function addItem() {
+        let input = document.getElementById("todoInput");
+        let text = input.value;
+        if (text === "") {
+          alert("Please enter a task");
+          return;
+        }
+        let li = document.createElement("li");
+        li.textContent = text;
+        let button = document.createElement("button");
+        button.textContent = "X";
+        button.onclick = function() {
+          removeItem(button);
+        };
+        li.appendChild(button);
+        document.getElementById("todoList").appendChild(li);
+        input.value = "";
+      }
+
+      function removeItem(button) {
+        let li = button.parentElement;
+        li.remove();
+      }
